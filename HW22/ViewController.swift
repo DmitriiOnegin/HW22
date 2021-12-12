ed7e78c885395b976ffd5d24c041a36c89481175
Dmitrii Onegin <dimimimo@mail.ru>
2021-12-12 18:57:02 +0300

    Add logic

--- HW22/ViewController.swift
+++ HW22/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redCountLabel: UILabel!
+    @IBOutlet weak var greenCountLabel: UILabel!
+    @IBOutlet weak var blueCountLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setLoadSlider()
+        setColorView()
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        setColorView()
+        redCountLabel.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        setColorView()
+        greenCountLabel.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        setColorView()
+        blueCountLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func setLoadSlider() {
+        redCountLabel.text = String(format: "%.2f", redSlider.value)
+        greenCountLabel.text = String(format: "%.2f", greenSlider.value)
+        blueCountLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func setColorView() {
+        let redSliderValue = CGFloat(redSlider.value)
+        let greenSliderValue = CGFloat(greenSlider.value)
+        let blueSliderValue = CGFloat(blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
+    }
 }
 
