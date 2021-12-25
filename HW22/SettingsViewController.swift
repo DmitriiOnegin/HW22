b78a76dcceea78bd0f342b31a026ff3a838a749d
Dmitrii Onegin <dimimimo@mail.ru>
2021-12-25 23:25:35 +0300

    Add logic

--- HW22/SettingsViewController.swift
+++ HW22/SettingsViewController.swift
@@ -16,9 +16,9 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
-    @IBOutlet weak var RedTextField: UITextField!
-    @IBOutlet weak var GreenTextField: UITextField!
-    @IBOutlet weak var BlueTextField: UITextField!
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
     
     var uiColor: UIColor!
     var delegate: SettingsViewControllerDelegate!
@@ -27,6 +27,8 @@ class SettingsViewController: UIViewController {
         super.viewDidLoad()
         
         setLoadSlider()
+        setLoadTextLabel()
+        setLoadTextField()
         setColorView()
     }
     
@@ -39,16 +41,19 @@ class SettingsViewController: UIViewController {
     @IBAction func redSliderAction() {
         setColorView()
         redCountLabel.text = string(from: redSlider)
+        redTextField.text = string(from: redSlider)
     }
     
     @IBAction func greenSliderAction() {
         setColorView()
         greenCountLabel.text = string(from: greenSlider)
+        greenTextField.text = string(from: greenSlider)
     }
     
     @IBAction func blueSliderAction() {
         setColorView()
         blueCountLabel.text = string(from: blueSlider)
+        blueTextField.text = string(from: blueSlider)
     }
     
     
@@ -62,16 +67,22 @@ class SettingsViewController: UIViewController {
         greenSlider.setValue(Float(greenValue), animated: true)
         blueSlider.setValue(Float(blueValue), animated: true)
         
-        setTextLabel()
+        
         
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
     }
     
-    private func setTextLabel() {
+    private func setLoadTextLabel() {
         redCountLabel.text = string(from: redSlider)
         greenCountLabel.text = string(from: greenSlider)
         blueCountLabel.text = string(from: blueSlider)
+    }
+    
+    private func setLoadTextField() {
+        redTextField.text = string(from: redSlider)
+        greenTextField.text = string(from: greenSlider)
+        blueTextField.text = string(from: blueSlider)
     }
         
     
@@ -86,6 +97,39 @@ class SettingsViewController: UIViewController {
     
     private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
+    }
+}
+
+extension SettingsViewController: UITextFieldDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let newValue = textField.text else { return  print("return1")}
+        guard let numberValue = Float(newValue) else { return print("return2")}
+        
+        switch textField {
+        case redTextField:
+            redSlider.setValue(numberValue, animated: true)
+            redCountLabel.text = string(from: redSlider)
+            print("redTextField")
+        case greenTextField:
+            greenSlider.setValue(numberValue, animated: true)
+            greenCountLabel.text = string(from: greenSlider)
+            print("greenTextField")
+        default:
+            blueSlider.setValue(numberValue, animated: true)
+            blueCountLabel.text = string(from: blueSlider)
+        }
+        setColorView()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
     }
 }
 
